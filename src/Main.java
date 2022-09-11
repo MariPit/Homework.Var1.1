eac6f69d71739aa789793cfd7153603e5e6e86a8
Мария <mariya_piterskaya@ymail.com>
2022-09-12 00:08:23 +0300

    Переменные 1. ДЗ 1. Задания 1-3

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задание 1
+        System.out.println ("Задание 1:");
+        var dog = 8.0;
+        System.out.println (dog);
+        var cat = 3.6;
+        System.out.println (cat);
+        var paper = 763789;
+        System.out.println (paper);
+
+        //Задание 2
+        System.out.println ("Задание 2:");
+        dog = dog + 4;
+        System.out.println (dog);
+        cat=cat+4;
+        System.out.println (cat);
+        paper=paper+4;
+        System.out.println (paper);
+
+        //Задание 3
+        System.out.println ("Задание 3:");
+        dog=dog-3.5;
+        System.out.println (dog);
+        cat=cat-1.6;
+        System.out.println (cat);
+        paper=paper-7639;
+        System.out.println (paper);
+
+    }
+}
